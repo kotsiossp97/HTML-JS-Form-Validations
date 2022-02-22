d62f613e2fb999d09aab225fc8d00117468a0248
kotsiossp97 <72583894+kotsiossp97@users.noreply.github.com>
2022-02-22 12:33:47 +0200

    Update ThreeTimeRange.js

--- ThreeTimeRange.js
+++ ThreeTimeRange.js
@@ -144,6 +144,9 @@ function updateTimeEnd(nextime) {
 
 
 // *********** HTML **************
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://plugins.slyweb.ch/jquery-clock-timepicker/jquery-clock-timepicker.min.js"></script>
+
 <input id="timeStart" type="text"/>
 <input id="timeEnd" type="text"/>
 <br><br>
